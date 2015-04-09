ac5449c5e56c182b4e130bda319c508b4e8766a8
Jimamba <jimstanley@jims-mbp.byod.gmu.edu>
2015-04-09 04:21:53 -0400

    Final Version

--- js/main.js
+++ js/main.js
@@ -17,28 +17,41 @@ window.onload = function() {
     
     function preload() {
         // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+        game.load.spritesheet( 'number', 'assets/number.png',100,100 );
+        game.load.image('background','assets/Background.png' );
+        game.load.image('winone','assets/winone.png');
+        game.load.image('wintwo','assets/wintwo.png');
+        game.load.audio('music','assets/Fully Stacked FINAL MIX.mp3')
+        
     }
     
-    var bouncy;
+    var number;
+    var background;
+    var player = 0;
+    var playertwo = 0;
+    var scoreone;
+    var scoretwo;
+    var stop;
+    var num = 0;
+    var turn = 1;
+    var winone;
+    var wintwo;
+    var music;
+     
     
     function create() {
+    	background = game.add.sprite(0,0,'background');
+    	music = game.add.audio('music');
+    	music.play();
         // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
+        number = game.add.sprite(350, 220, 'number' );
+        number.animations.add('cycle',[0,1,2,3,4,5,6],10,true);
         
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something awesome.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+    scoreone = game.add.text(50,500, '0',{ fontSize: '30px', fill: '#000'});
+    scoretwo = game.add.text(710,500, '0',{ fontSize: '30px', fill: '#000'});
+    stop = game.input.keyboard.addKey(Phaser.Keyboard.S);
     }
     
     function update() {
@@ -47,6 +60,137 @@ window.onload = function() {
         // in X or Y.
         // This function returns the rotation angle that makes it visually match its
         // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
+        if(player == 21 && playertwo != 21)
+        {
+		winone = game.add.sprite(0,0,'winone');
+		music.stop();
+		game.paused = true;     
+        }
+        if(playertwo == 21 && player != 21)
+        {
+        wintwo = game.add.sprite(0,0,'wintwo');
+        music.stop();
+		game.paused = true;     
+        }
+       
+        number.animations.play('cycle',10);
+        if(turn == 1)
+        {
+        if(stop.isDown)
+        {
+        if(number.frame == 0)
+        {
+        if(player < 21)
+        {
+        player = player + 1;
+        }
+        }
+        else if(number.frame == 1)
+        {
+        if(player < 21)
+        {
+        player = player + 2;
+        }
+        }
+        else if(number.frame == 2)
+        {
+        if(player < 21)
+        {
+        player = player + 3;
+        }
+        }
+         else if(number.frame == 3)
+        {
+        if(player < 21)
+        {
+        player = player + 4;
+        }
+        }
+        else if(number.frame == 4)
+        {
+        if(player < 21)
+        {
+        player = player + 5;
+        }
+        }
+         else if(number.frame == 5)
+        {
+        if(player < 21)
+        {
+        player = player + 6;
+        }
+        }
+        else if(number.frame == 6)
+        {
+        if(player < 21)
+        {
+        player = player + 7;
+        }
+        }
+        scoreone.text = '' + player;
+        
+        }
+        turn = 2;
+        }
+        else
+        {
+        if(stop.isDown)
+        {
+        if(number.frame == 0)
+        {
+        if(playertwo < 21)
+        {
+        playertwo = playertwo + 1;
+        }
+        }
+        else if(number.frame == 1)
+        {
+        if(playertwo < 21)
+        {
+        playertwo = playertwo + 2;
+        }
+        }
+        else if(number.frame == 2)
+        {
+        if(playertwo < 21)
+        {
+        playertwo = playertwo + 3;
+        }
+        }
+         else if(number.frame == 3)
+        {
+        if(playertwo < 21)
+        {
+        playertwo = playertwo + 4;
+        }
+        }
+        else if(number.frame == 4)
+        {
+        if(playertwo < 21)
+        {
+        playertwo = playertwo + 5;
+        }
+        }
+         else if(number.frame == 5)
+        {
+        if(playertwo < 21)
+        {
+        playertwo = playertwo + 6;
+        }
+        }
+        else if(number.frame == 6)
+        {
+        if(playertwo < 21)
+        {
+        playertwo = playertwo + 7;
+        }
+        }
+        scoretwo.text = '' + playertwo;
+        
+        }
+        turn = 1;
+        }
+        
+        number.animations.play('cycle');
     }
 };
